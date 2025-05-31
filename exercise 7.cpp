9f5ae3d005b962194d444209e81e7c27f5264a99
Киала Ибаньес Армандо ТСА-141 <quialaibaneza@gmail.com>
2025-05-31 19:13:04 +0300

    Update exercise 7.cpp

--- exercise 7.cpp
+++ exercise 7.cpp
@@ -1,32 +1,60 @@
-
 #include <iostream>
 #include <iomanip>
 #include <ctime>
 
+bool checkCondition(int** array, const size_t m, const size_t n);
+
 int getValue();
-
+/**
+ * @brief считывает вещественное число с клавиатуры с проверкой ввода
+ * @return считанное значение
+*/
 size_t getSize();
-
+/**
+ * @brief считывает вещественное число с клавиатуры с проверкой ввода
+ * @return считанное значение
+*/
 void checkN(const int n);
-
+/**
+ * @brief проверяет если число введен верно
+*/
 int** getNewArray(const size_t m, const size_t n);
-
+/**
+ * @brief Сохранение пространства в памяти для создания нового массива
+*/
 void printArray(int** array, const size_t m, const size_t n);
-
+/**
+ * @brief Печать массива
+*/
 void fillArray(int** array, const size_t m, const size_t n);
-
-void changeNegativeElemen(int** array, const size_t m, const size_t n);
-
+/**
+ * @brief Пополнение массива с клавиатурой
+*/
+void changeNegativeElement(int** array, const size_t m, const size_t n);
+/**
+ * @brief Заменить отрицательный элемент каждого столбца нулем
+*/
 void deleteArray(int** array, const size_t m, const size_t n);
-
+/**
+ * @brief Удалит массив для выпуска памяти
+*/
 int** copyArray(int** array, const size_t m, const size_t n);
-
+/**
+ * @brief Делает копию исходного массива
+ * @return новый массив
+*/
 int getCountOfColumns(int** array, const size_t m, const size_t n);
-
+/**
+ * @brief Читает число столбцов,содержащих элемент,кратный пяти
+*/
 void deleteColumns(int** array, const size_t m, const size_t n, int** newarray);
-
+/**
+ * @brief Удалить каждый столбец, содержащий элемент, кратный пяти
+*/
 void fillRandom(int** array, const size_t m, const size_t n, const int start, const int end);
-
+/**
+ * @brief Пополнение массива рандомно
+*/
 using namespace std;
 
 enum {RANDOM = 5, MANUAL =6};
@@ -49,8 +77,10 @@ int main()
                         start = getValue();
                         cout<< "Enter end: ";
                         end = getValue();
-                        fillRandom(array, m, n, start, end);
+                        if(start<end){
+						fillRandom(array, m, n, start, end);
                         break;
+						}
         case MANUAL:    fillArray(array,m,n);
                         break;
         default:
@@ -61,14 +91,15 @@ int main()
     printArray(array, m, n);
     cout<<endl;
     int** arrayCopy = copyArray(array,m,n);
-    changeNegativeElemen(arrayCopy,m,n);
-    
+    changeNegativeElement(arrayCopy,m,n);
+    cout<<"After changing the negative elements by 0"<<endl;
     printArray(arrayCopy,m,n);
     deleteArray(arrayCopy,m,n);
     
-    size_t newn = n + getCountOfColumns(array,m,n);
+    size_t newn = n - getCountOfColumns(array,m,n);
     int** resultarray = getNewArray(m,newn);
     deleteColumns(array,m,n,resultarray);
+    cout<<"After removing the columns with multiples of 5("<<newn<<"columns remain):"<<endl;
     printArray(resultarray,m,newn);
     deleteArray(resultarray,m,newn);
     deleteArray(array,m,n);
@@ -164,32 +195,34 @@ void changeNegativeElement(int** array, const size_t m, const size_t n)
 }
 }
 
+bool checkCondition(int** array, const size_t m, const size_t col){
+	for (size_t i=0; i<m;i++)
+    {
+		if (array [i][col]%5 == 0){
+			return true;
+		}
+		}
+return false;
+}
+
+
+
 void deleteColumns(int** array, const size_t m, const size_t n, int** newarray)
 {
-    size_t newj = 0;
+    size_t newCol = 0;
     for (size_t j = 0; j<n; j++)
     {
-        for (size_t i = 0; i<m; i++ )
+        if (!checkCondition(array, m, j)){
+		    for (size_t i = 0; i<m; i++ )
         {
-            newarray[i][newj] = array [i][j];
-            
-        }
-        for (size_t i = 0; i<m; i++)
-        {
-            if (array [i][j]%5 == 0)
-            {
-                for (size_t k=0; k<m; k++)
-                {
-                    newarray[k][newj+1] = 0;
-                }
-                newj++;
-                break;
-            }
-            
-        }
-        newj++;
-    }
+        	newarray[i][newCol] = array [i][j];
+		}
+			newCol++;
+	}
+ }
 }
+            
+        
 int** copyArray(int** array, const size_t m, const size_t n)
 {
     int** result = getNewArray(m,n);
@@ -217,21 +250,13 @@ void deleteArray(int** array, const size_t m, const size_t n)
     {
         int result = 0;
         for (size_t j = 0; j<n; j++)
-        {
-        for (size_t i = 0; i<m; i++)
-        {
-            if (array [i][j] %5== 0)
+        {  
+           if ((checkCondition(array, m, j)))
             {
                 result++;
-                break;
             }
             
         }
-        }
         return result;
     }
-
-
-    
-   
-
+        
